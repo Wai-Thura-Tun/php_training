f468bb09c2d2a8093a806ebae77db4c8d2ddf732
Thura <waithura13579@gmail.com>
2022-04-22 14:06:17 +0630

    Fixing carelsss mistakes for tutorial3.

--- tutorials/tutorial3/index.php
+++ tutorials/tutorial3/index.php
@@ -10,33 +10,35 @@
     <div class="container">
         <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
             <label>Choose Date To Calculate Age</label>
-            <input type="date" placeholder="Pick Date" name="dob">
+            <input type="date" placeholder="Pick Date" name="dob" required>
             <button>Submit</button>
         </form>
         <div class="resultContainer">
             <?php
-            $userDOB = "";
-            if (isset($_POST['dob'])) {
-                $userDOB = $_POST['dob'];
-            }
             function calculateDOB($dob)
             {
                 $currentMonth = (int)date('m');
                 $currentYear = (int)date('Y');
                 $currentDay = (int)date('d');
                 $dateArray = explode('-', $dob, 3);
-                $userYear = (int)$dateArray[0];
-                $userMonth = (int)$dateArray[1];
-                $userDay = (int)$dateArray[2];
+                $userYear = isset($dateArray[0]) ? (int)$dateArray[0] : 0;
+                $userMonth = isset($dateArray[1]) ? (int)$dateArray[1] : 0;
+                $userDay = isset($dateArray[2]) ? (int)$dateArray[2] : 0;
                 $diffYear = $currentYear - $userYear;
                 $diffDay = $currentDay - $userDay;
                 $diffMonth = $currentMonth - $userMonth;
                 $totalDay = ((($diffYear * 12) + $diffMonth) * 30) + $diffDay;
-                $userAge = $totalDay / 365;
-                return ceil($userAge);
+                $userAge = ceil($totalDay / 365);
+                if ($userAge <= 0 || $userAge == -0) {
+                    return "Invalid Date Of Birth or You're not born yet!";
+                } else {
+                    return $userAge;
+                }
             }
             ?>
-            Your age is : <span><?php echo calculateDOB($userDOB); ?></span>
+            <?php if (isset($_POST['dob'])) { ?>
+                Your age is : <span><?php echo calculateDOB($_POST['dob']); ?></span>
+            <?php } ?>
         </div>
     </div>
 </body>
