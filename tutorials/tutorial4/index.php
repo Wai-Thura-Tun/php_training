0c116eae275317b518850686750d1ad4ff650e61
Thura <waithura13579@gmail.com>
2022-04-22 16:26:12 +0630

    Adding CSS Folder.

--- tutorials/tutorial4/index.php
+++ tutorials/tutorial4/index.php
@@ -5,10 +5,10 @@ session_start();
 <html>
 
 <head>
-    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="css/reset.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
