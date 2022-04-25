98519efb9946f8f25e609a8790ed98b1c90de9c8
Thura <waithura13579@gmail.com>
2022-04-25 14:06:57 +0630

    Updating Tutorial5.

--- tutorials/tutorial5/index.php
+++ tutorials/tutorial5/index.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpWord\IOFactory as PHPWordIOFact ;
 
 /**
  * Summary of readFiles
@@ -19,8 +20,8 @@ function readFiles($type = "txt", $fileName)
         fclose($fileResource);
         return $fileContent ?? null;
     } else if ($fileExten == "doc") {
-        $phpWord = \PhpOffice\PhpWord\IOFactory::load($fileName, 'MsDoc');
-        $word = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, "HTML")->save("php://output");
+        $phpWord = PHPWordIOFact::load($fileName, 'MsDoc');
+        $word = PHPWordIOFact::createWriter($phpWord, "HTML")->save("php://output");
     } else {
         $reader = IOFactory::createReader(ucfirst(strtolower($type)));
         $spreadSheet = $reader->load($fileName);
@@ -28,8 +29,10 @@ function readFiles($type = "txt", $fileName)
         $message = $write->save("php://output");
     }
 }
+echo "<h1>CSV File Content is </h1><br>";
 echo readFiles("CSV", "files/test4.csv");
+echo "<h1>Excel File Content is </h1><br>";
 echo readFiles("XLS", "files/test1.xls");
 echo "<h1>Text File Content is</h1>" . readFiles("TXT", "files/test2.txt") . "<br>";
-echo "<br>";
+echo "<h1>Doc File Content is </h1><br>";
 echo readFiles("TXT", "files/test3.doc");
